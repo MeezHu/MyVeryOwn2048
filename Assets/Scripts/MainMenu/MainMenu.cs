fcaa4574118ad4cd051576ced01e7421db54bdfc
MeezHu <93200594+MeezHu@users.noreply.github.com>
2024-02-20 15:22:00 +0100

    Canvas and script
    start

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PU_Go2048scene()
+    {
+        Debug.Log("Start");
+        //SceneManager.LoadSceneAsync(1);
+    }
+    
+}
