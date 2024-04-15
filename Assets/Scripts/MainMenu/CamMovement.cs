7c1bbc3600221ee2f4f8a0f0d7723310adbae992
MeezHu <93200594+MeezHu@users.noreply.github.com>
2024-04-15 15:53:39 +0200

    CameraMenu
    Responds properly
    Animations work properly
    
    Will need to touch the curves (vanillas are dogshit)

---
+++ Assets/Scripts/MainMenu/CamMovement.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    public Animator cam;
+    
+    [SerializeField] private bool canOptPanOut;
+    [SerializeField] private bool canQuiPanOut;
+    [SerializeField] private bool canStaPanOut;
+    [SerializeField] private bool canStart;
+    
+    //================================END OF VARIABLES==============================================
+
+    void Start()
+    {
+        //cam.speed = 0;
+        canOptPanOut = false;
+        canStaPanOut = false;
+        canQuiPanOut = false;
+        canStart = false;
+
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !canOptPanOut)
+        {
+            Debug.Log("Options");
+            OptionPanel();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Backspace) && canOptPanOut)
+        {
+            Debug.Log("Quit Options");
+            OptionPanelOut();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !canQuiPanOut)
+        {
+            Debug.Log("Wanna Quit?");
+            QuitPanel();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Backspace) && canQuiPanOut)
+        {
+            Debug.Log("Quit Quit");
+            QuitPanelOut();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !canStaPanOut)
+        {
+            Debug.Log("Wanna Play?");
+            MoveStartPanel();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Backspace) && canStaPanOut && canStart)
+        {
+            Debug.Log("Quit Play");
+            MoveStartPanelOut();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && canStaPanOut && canStart)
+        {
+            Debug.Log("Play");
+            MoveStart();
+        }
+    }
+
+
+    // public void StartPanel()
+    // {
+    //     cam.SetTrigger("");
+    // }
+    
+    public void MoveStartPanel()
+    {
+        cam.SetTrigger("MoveStartPanel");
+        canStaPanOut = true;
+        StartCoroutine(CanStartSecurity());
+    }
+    
+    public void MoveStartPanelOut()
+    {
+        cam.SetTrigger("MoveStartPanelOut");
+        canStaPanOut = false;
+        canStart = false;
+    }
+    
+    public void MoveStart()
+    {
+        cam.SetTrigger("MoveStart");
+        canStart = false;
+    }
+    
+    public void OptionPanel()
+    {
+        cam.SetTrigger("OptionPanel");
+        canOptPanOut = true;
+    }
+
+    public void OptionPanelOut()
+    {
+        cam.SetTrigger("OptionPanelOut");
+        canOptPanOut = false;
+    }
+    
+    public void QuitPanel()
+    {
+        cam.SetTrigger("QuitPanel");
+        canQuiPanOut = true;
+    }
+    
+    public void QuitPanelOut()
+    {
+        cam.SetTrigger("QuitPanelOut");
+        canQuiPanOut = false;
+    }
+
+    private IEnumerator CanStartSecurity()
+    {
+        yield return new WaitForSeconds(1f);
+        canStart = true;
+
+        yield return null;
+    }
+}
