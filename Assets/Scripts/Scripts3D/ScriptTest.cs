6448764a1c62d7c96f1e754d0c252ce83a368d0b
MeezHu <93200594+MeezHu@users.noreply.github.com>
2024-08-19 18:17:39 +0200

    Intégration Wwise fonctionnel avec le premier son

--- Assets/Scripts/Scripts3D/ScriptTest.cs
+++ Assets/Scripts/Scripts3D/ScriptTest.cs
@@ -1,9 +1,12 @@
+using AK.Wwise;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScriptTest : MonoBehaviour
 {
+    public Switch material;
+
     // Start is called before the first frame update
     void Start()
     {
